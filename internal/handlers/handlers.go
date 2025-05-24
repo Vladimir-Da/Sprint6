5de790642fc04b9caa274cef37b9c29e7b320001
Владимир Данилов <Nevrak87@gmail.com>
2025-05-24 18:10:53 +0300

    final version

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,70 @@
 package handlers
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+func IndexHandler(w http.ResponseWriter, req *http.Request) {
+
+	dataHtml, err := os.ReadFile("index.html")
+	if err != nil {
+		http.Error(w, "index.html read error", http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusOK)
+	// записываем html данные в тело ответа
+	w.Write(dataHtml)
+}
+func MainHandler(w http.ResponseWriter, r *http.Request) {
+
+	// Парсинг МультиПартФормы
+	err := r.ParseMultipartForm(10)
+	if err != nil {
+		http.Error(w, "ParseMultipartForm error", http.StatusInternalServerError)
+		return
+	}
+	// парсинг файла (name="myFile") из body
+	file, fileheader, err := r.FormFile("myFile")
+	if err != nil {
+		http.Error(w, "FormFile error", http.StatusInternalServerError)
+		return
+	}
+	// отложенное закрытие файла
+	defer file.Close()
+
+	fileBytes, err := io.ReadAll(file)
+	if err != nil {
+		http.Error(w, "io.ReadAll error", http.StatusInternalServerError)
+		return
+	}
+	translated, err := service.Translate(string(fileBytes))
+	if err != nil {
+		http.Error(w, "Morse translate error", http.StatusInternalServerError)
+		return
+	}
+	fileName := time.Now().UTC().Format("2025-12-01")
+	fileExt := filepath.Ext(fileheader.Filename)
+	fileNameRes := fmt.Sprintf("%s%s", fileName, fileExt)
+	localFileile, err := os.Create(fileNameRes)
+	if err != nil {
+		http.Error(w, "local file creat error", http.StatusInternalServerError)
+		return
+	}
+	defer localFileile.Close()
+	_, err = localFileile.WriteString(translated)
+	if err != nil {
+		http.Error(w, "local file write error", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(translated))
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,36 @@
 package server
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/handlers"
+	"github.com/go-chi/chi"
+)
+
+type Server struct {
+	Log        *log.Logger
+	HttpServer *http.Server
+}
+
+func NewServer(log *log.Logger) *Server {
+	r := chi.NewRouter()
+	r.Get("/", handlers.IndexHandler)
+	r.Post("/upload", handlers.MainHandler)
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		fmt.Printf("Start server error: %s", err.Error())
+
+	}
+
+	httpServer := &http.Server{
+		Addr:         ":8080",
+		Handler:      r,
+		ErrorLog:     log,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+	return &Server{Log: log, HttpServer: httpServer}
+}
